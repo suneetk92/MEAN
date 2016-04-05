8203fb6873960ba364922888db5a4dbfa2085a84
Suneet Kumar <suneetk92@gmail.com>
2016-04-05 19:18:51 +0100

    Removing cache as it was not showing correct name

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -39,7 +39,7 @@ angular.module('authService', [])
 	// Get the user info
 	authFactory.getUser = function () {
 		if (AuthToken.getToken()) {
-			return $http.get('/api/me', { cache : true });
+			return $http.get('/api/me');
 		} else {
 			return $q.reject({ message: 'User has no token.'});
 		}
